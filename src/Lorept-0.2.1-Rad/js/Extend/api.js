9816d0c246324e2533017ecd66b5c16783bdc767
陆御&春风展望 <111875719+Buelie@users.noreply.github.com>
2022-12-14 14:38:50 +0800

    Update api.js

--- src/Lorept-0.2.1-Rad/js/Extend/api.js
+++ src/Lorept-0.2.1-Rad/js/Extend/api.js
@@ -1,1 +1,10 @@
-
+//使用import语句导入扩展（例：import 'hello.js'）
+import '../main.js'
+export function alert(){
+	var value;
+	alert(value)
+}
+export function aaddition(one , two){
+	var outcome;
+	outcome = one + two + '';
+}
