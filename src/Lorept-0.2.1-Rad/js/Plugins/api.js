8baa3149d663cbcb696080326bf3d532b40bfa2d
陆御&春风展望 <111875719+Buelie@users.noreply.github.com>
2022-12-17 16:51:24 +0800

    Update api.js

--- src/Lorept-0.2.1-Rad/js/Plugins/api.js
+++ src/Lorept-0.2.1-Rad/js/Plugins/api.js
@@ -1,1 +1,29 @@
+//使用import语句导入插件（例：import 'hello.js'）
 import '../main.js'
+
+//在此导入扩展或插件
+
+export function notice(one){
+	document.getElementById("notice").innerHTML=one
+	return 0;
+}
+export function addition(one , two){
+	var outcome;
+	outcome = one + two + '';
+	return 0;
+}
+export function subtraction(one ,two){
+	var outcome;
+	outcome = one - two + '';
+	return 0;
+}
+export function multiplication(one , two){
+	var outcome;
+	outcome = one * two + '';
+	return 0;
+}
+export function division(one , two){
+	var outcome;
+	outcome = one / two + '';
+	return 0;
+}
